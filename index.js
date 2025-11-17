dc8e965fd121059f926a63fb627332207f1011d4
ViniciusCassemira <vinicius.cassemira@fatec.sp.gov.br>
2025-11-17 15:24:04 -0300

    🐛 fix: fixing some bugs to use compose and k8s

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use('/', public_routes);
 app.use('/', private_routes);
 
-const SERVER_PORT = process.env.PORT || 3000;
+const SERVER_PORT = 3000;
 
 app.listen(SERVER_PORT, () => {
   console.log(`The server is running on ${SERVER_PORT}`);
